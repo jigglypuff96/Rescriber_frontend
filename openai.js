195e9a6e95bb168c94369dfe983c6f4cb4e062f7
Tianshi Li <9413fi@gmail.com>
2024-07-14 23:24:15 -0400

    change the model to gpt-4o

--- openai.js
+++ openai.js
@@ -59,7 +59,7 @@ export async function getCloudResponseDetect(userMessageDetect) {
   };
 
   const body = JSON.stringify({
-    model: "gpt-4-turbo",
+    model: "gpt-4o",
     messages: [
       { role: "system", content: systemPromptDetect },
       { role: "user", content: userMessageDetect },
