7d067170ab45121ac57880984609825e1f693189
jigglypuff96 <seazon96zhou@gmail.com>
2024-11-19 00:13:39 -0500

    add port + changed result format

--- ondevice.js
+++ ondevice.js
@@ -1,5 +1,5 @@
 export async function getOnDeviceResponseDetect(userMessageDetect) {
-  const response = await fetch("https://llmstudy.peach.codes/detect", {
+  const response = await fetch("https://llmstudy.peach.codes:5000/detect", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -14,18 +14,18 @@ export async function getOnDeviceResponseDetect(userMessageDetect) {
     if (data.results == "") {
       return;
     }
-    jsonObject = JSON.parse(data.results);
+    jsonObject = data.results;
   } else {
     if (data == "") {
       return;
     }
-    jsonObject = JSON.parse(data);
+    jsonObject = data;
   }
-  return jsonObject.results;
+  return jsonObject;
 }
 
 export async function getOnDeviceResponseCluster(userMessageCluster) {
-  const response = await fetch("https://llmstudy.peach.codes/cluster", {
+  const response = await fetch("https://llmstudy.peach.codes:5000/cluster", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -45,7 +45,7 @@ export async function getOnDeviceAbstractResponse(
   const userMessage = `<Text>${currentMessage}</Text>\n<ProtectedInformation>${abstractList.join(
     ", "
   )}</ProtectedInformation>`;
-  const response = await fetch("https://llmstudy.peach.codes/abstract", {
+  const response = await fetch("https://llmstudy.peach.codes:5000/abstract", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -54,6 +54,5 @@ export async function getOnDeviceAbstractResponse(
   });
   const data = await response.json();
   const resultString = data.results;
-  const jsonObject = JSON.parse(resultString);
-  return jsonObject.results;
+  return resultString;
 }
